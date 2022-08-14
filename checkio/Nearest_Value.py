f18f978593bee227d2c0017b90c58e10ff8f12a1
senmm <mobilehoneypot0.1@gmail.com>
2022-08-14 08:26:08 +0000

    Added a Nearest_Value.py.

---
+++ checkio/Nearest_Value.py
@@ -0,0 +1,32 @@
+def nearest_value(values: set, one: int) -> int:
+    temp_value = 9999999
+    for word in values:
+        word = int(word)
+        temp_abs = abs(one - word)
+        if word == one:
+            return(one)
+        elif temp_abs < temp_value:
+            temp_value = temp_abs
+            temp = word
+        elif temp_abs == temp_value:
+            if temp > word:
+                temp = word
+    return(temp)
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(nearest_value({4, 7, 10, 11, 12, 17}, 9))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert nearest_value({4, 7, 10, 11, 12, 17}, 9) == 10
+    assert nearest_value({4, 7, 10, 11, 12, 17}, 8) == 7
+    assert nearest_value({4, 8, 10, 11, 12, 17}, 9) == 8
+    assert nearest_value({4, 9, 10, 11, 12, 17}, 9) == 9
+    assert nearest_value({4, 7, 10, 11, 12, 17}, 0) == 4
+    assert nearest_value({-6, -2, 4, 7, 12, 17}, -4) == -6
+    assert nearest_value({4, 7, 10, 11, 12, 17}, 100) == 17
+    assert nearest_value({5, 10, 8, 12, 89, 100}, 7) == 8
+    assert nearest_value({5}, 5) == 5
+    assert nearest_value({5}, 7) == 5
+    print("Coding complete? Click 'Check' to earn cool rewards!")
