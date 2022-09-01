51b849455d1a1e441a9a60607fa094a7c7a30620
Sen <63241411+senmm@users.noreply.github.com>
2022-09-01 18:05:30 +0900

    Create The_Most_Frequent.py

---
+++ checkio/The_Most_Frequent.py
@@ -0,0 +1,12 @@
+def most_frequent(data: list[str]) -> str:
+    from statistics import mode
+    return mode(data)
+
+
+print("Example:")
+print(most_frequent(["a", "b", "c", "a", "b", "a"]))
+
+assert most_frequent(["a", "b", "c", "a", "b", "a"]) == "a"
+assert most_frequent(["a", "a", "bi", "bi", "bi"]) == "bi"
+
+print("The mission is done! Click 'Check Solution' to earn rewards!")
